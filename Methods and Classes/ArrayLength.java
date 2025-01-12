a1a147a8126c06bad0df870e88c6c555ff99c2da
ROBOT X <w3aarush@users.noreply.github.com>
2025-01-12 23:50:30 +0530

    Create ArrayLength.java

---
+++ Methods and Classes/ArrayLength.java
@@ -0,0 +1,12 @@
+// This program demonstrates the length array member.
+class Length {
+    public static void main(String args[]) {
+        int a1[] = new int[10];
+        int a2[] = {3, 5, 7, 9, 11, 13};
+        int a3[] = {2,4,6,8,10};
+
+        System.out.println("Length of a1 is " + a1.length);
+        System.out.println("Length of a2 is " + a2.length);
+        System.out.println("Length of a3 is " + a3.length);
+    }
+}
